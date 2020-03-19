e4549e2a73fae7b409597e0aec36523a8c8767c0
nelson <nelson.mizutani@hotmail.com>
2020-03-19 17:04:17 -0300

    Criados atributos de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,13 +1,19 @@
 class Pessoa:
+    def __init__(self, nome=None,
+                 idade=50):  # o metodo __init__ inicializa os atributos de dados
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):         # na criação com parâmetro já define o próprio objeto self.
         return f'Ola {id(self)}'    # verifica-se o mesmo objeto pelo id
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Nelson')
     print(Pessoa.cumprimentar(p))
-
     print(id(p))
-
-    print(p.cumprimentar())     # nao precisa passar o proprio objeto no parâmetro self
-    # print(p.cumprimentar(7))    # se passar qualquer parâmetro tem erro de 2 parâmetros
+    print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Mizutani'
+    print(p.nome)
+    print(p.idade)
