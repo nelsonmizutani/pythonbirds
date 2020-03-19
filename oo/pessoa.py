eab09553dd8ea0dcdaf5932e5781e7dcf6aaa29d
nelson <nelson.mizutani@hotmail.com>
2020-03-19 18:38:02 -0300

    Criados e removidos atributos em instâncias de Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,19 +1,28 @@
 class Pessoa:
-    def __init__(self, nome=None,
-                 idade=50):  # o metodo __init__ inicializa os atributos de dados
-        self.idade = idade
+    # o metodo __init__ inicializa os atributos de dados
+    def __init__(self
+                 ,*filhos
+                 ,nome=None
+                 ,idade=30
+                 ):
         self.nome = nome
+        self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):         # na criação com parâmetro já define o próprio objeto self.
         return f'Ola {id(self)}'    # verifica-se o mesmo objeto pelo id
 
 
 if __name__ == '__main__':
-    p = Pessoa('Nelson')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Mizutani'
-    print(p.nome)
-    print(p.idade)
+    kazuo = Pessoa(nome='Kazuo')
+    akira = Pessoa(nome='Akira')
+    nelson = Pessoa(kazuo,akira, nome='Nelson')
+    print(nelson.cumprimentar())
+    print(nelson.nome)
+    print(nelson.idade)
+    for filho in nelson.filhos:
+        print(filho.nome)
+    nelson.sobrenome = 'Mizutani'
+    del nelson.filhos
+    print(nelson.__dict__)
+    print(kazuo.__dict__)
