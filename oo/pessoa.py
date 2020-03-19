ceb68a28c19039c1c039ba62487273c13939cf53
mrgod <mrgod@outlook.com.br>
2020-03-19 16:17:27 -0300

    Criado metodo cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,13 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return f'Ola {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+
+    print(id(p))
+
+    print(p.cumprimentar())     # nao precisa passar o proprio objeto no parâmetro self
+    # print(p.cumprimentar(7))    # se passar qualquer parâmetro tem erro de 2 parâmetros
