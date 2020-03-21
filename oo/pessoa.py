b99b25353f12ebe22bcb45852f1eee51586ef1c2
nelson <nelson.mizutani@hotmail.com>
2020-03-20 23:08:14 -0300

    Criados métodos protegidos para status de fase

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,8 +7,8 @@ class Pessoa:
         self.idade = idade
         self.filhos = list(filhos)
 
-    def cumprimentar(self):  # na criação com parâmetro já define o próprio objeto self.
-        return f'Ola {id(self)}'  # verifica-se o mesmo objeto pelo id
+    def cumprimentar(self):
+        return f'Ola, meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -18,24 +18,29 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+class Homem(Pessoa):
+    def cumprimentar(self):
+        # cumprimentar_da_classe=self.cumprimentar() # isto gera loop ao chamar ela mesma
+        cumprimentar_da_classe = super().cumprimentar()
+        return f' {cumprimentar_da_classe}. Aperto de mão'
+
+
+class Mutante(Pessoa):
+     olhos = 3
+
 if __name__ == '__main__':
-    kazuo = Pessoa(nome='Kazuo')
-    akira = Pessoa(nome='Akira')
-    nelson = Pessoa(kazuo,akira, nome='Nelson')
+    akira = Homem(nome='Akira')
+    kazuo = Mutante(nome='Kazuo')
+    nelson = Homem(kazuo,akira, nome='Nelson')
     print(nelson.nome)
     print(nelson.idade)
     for filho in nelson.filhos:
         print(filho.nome)
     del nelson.filhos
-    kazuo.olhos  = 1
     print(nelson.__dict__)
     print(kazuo.__dict__)
     print(Pessoa.olhos)     # referência direta na classe
     print(nelson.olhos)     # referência no objeto
     print(kazuo.olhos)     # referência no objeto
-    print(id(Pessoa.olhos), id(kazuo.olhos))   # o id é o mesmo
-    Pessoa.olhos = 3
-    print(Pessoa.olhos)     # referência direta na classe
-    print(Pessoa.metodo_estatico(), kazuo.metodo_estatico())
-    print(Pessoa.nome_e_atributos_de_classe(), kazuo.nome_e_atributos_de_classe())
-
+    print(nelson.cumprimentar())
+    print(kazuo.cumprimentar())
